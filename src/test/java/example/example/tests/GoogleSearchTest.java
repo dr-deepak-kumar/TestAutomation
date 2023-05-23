d80c56de9db5d3525e0a9f8cf058f5cdf601e3d1
huft <huft@DESKTOP-SS9KUUU>
2023-05-23 16:23:16 +0530

    Disabled Test

--- src/test/java/example/example/tests/GoogleSearchTest.java
+++ src/test/java/example/example/tests/GoogleSearchTest.java
@@ -1,12 +1,12 @@
 package example.example.tests;
-import org.testng.ITestResult;
+//import org.testng.ITestResult;
 
 import org.testng.Reporter;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import app.getxray.xray.testng.annotations.Requirement;
-import app.getxray.xray.testng.annotations.XrayTest;
+//import app.getxray.xray.testng.annotations.Requirement;
+//import app.getxray.xray.testng.annotations.XrayTest;
 import example.example.factory.PageinstancesFactory;
 import example.example.pages.GooglePage;
 
@@ -22,13 +22,12 @@ public class GoogleSearchTest extends BaseTest {
 	 * Google search test.
 	 */
 	@Test
-	@XrayTest(key="TAIS-25")
 	public void googleSearchTest1() {
 		driver.get("https://www.google.co.in/");
 		GooglePage googlePage = PageinstancesFactory.getInstance(GooglePage.class);
 		googlePage.searchText("abc");
 		Assert.assertTrue(driver.getTitle().contains("abc"), "Title doesn't contain abc : Test Failed");
-		ITestResult result = Reporter.getCurrentTestResult();
+	//	ITestResult result = Reporter.getCurrentTestResult();
 		
 	}
 	@Test
@@ -37,7 +36,7 @@ public class GoogleSearchTest extends BaseTest {
 		GooglePage googlePage = PageinstancesFactory.getInstance(GooglePage.class);
 		googlePage.searchText("abc");
 		Assert.assertTrue(driver.getTitle().contains("abc"), "Title doesn't contain abc : Test Failed");
-	    ITestResult result = Reporter.getCurrentTestResult();
+	  //  ITestResult result = Reporter.getCurrentTestResult();
 		
 	}
 }
