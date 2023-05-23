34bea1f5ec3f30c0f65608590d7e14309fa7eb47
huft <huft@DESKTOP-SS9KUUU>
2023-05-23 15:03:32 +0530

    Added Requirements in the Code

--- src/test/java/example/example/tests/GoogleSearchTest.java
+++ src/test/java/example/example/tests/GoogleSearchTest.java
@@ -4,6 +4,7 @@ import org.testng.Reporter;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import app.getxray.xray.testng.annotations.Requirement;
 import app.getxray.xray.testng.annotations.XrayTest;
 import example.example.factory.PageinstancesFactory;
 import example.example.pages.GooglePage;
@@ -20,7 +21,8 @@ public class GoogleSearchTest extends BaseTest {
 	 * Google search test.
 	 */
 	@Test
-	@XrayTest(key = "TAIS-20")	
+	@Requirement(key = "TASP-10")
+	@XrayTest(key = "TAIS-25")	
 	public void googleSearchTest() {
 		driver.get("https://www.google.co.in/");
 		GooglePage googlePage = PageinstancesFactory.getInstance(GooglePage.class);
